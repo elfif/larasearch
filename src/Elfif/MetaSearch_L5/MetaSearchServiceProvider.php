446ebc0c7c73b91037c4be1b9f3c3883aa6ffb7f
Quentin Basquin <qbasquin@immoone.fr>
2017-11-24 16:45:51 +0100

    compatibilité laravel 5.5

--- src/Elfif/MetaSearch_L5/MetaSearchServiceProvider.php
+++ src/Elfif/MetaSearch_L5/MetaSearchServiceProvider.php
@@ -1,47 +1,31 @@
-<?php namespace Elfif\MetaSearch_L5; 
+<?php
+
+namespace Elfif\MetaSearch_L5;
 
 use Illuminate\Support\ServiceProvider;
 
 class MetaSearchServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-            
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return voids
-	 */
-	public function register()
-	{
-            $this->app['metasearch'] = $this->app->share(function($app)
-            {
-                return new MetaSearch();
-            });
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-            return array('metasearch');
-	}
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('metasearch', function($app){
+            return new MetaSearch();
+        });
+    }
 
 }
